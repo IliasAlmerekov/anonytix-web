e2a55c3d34b0e7b32e0011596ec89dc83ab65bca
ilias.almerekov <ilias.almerekov@db-n.com>
2026-06-06 16:31:56 +0200

    feat: theme switcher (light/dark/system) in sidebar account menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from 'next-themes'
 import './index.css'
 import App from './App.tsx'
 import SurveysListPage from '@/pages/SurveysListPage.tsx'
@@ -26,6 +27,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
